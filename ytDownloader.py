8b79dc8f3dd5e5db1b151c74ba5be79b4e9e8144
everctba <everson777@gmail.com>
2024-05-26 02:33:45 -0300

    baixador de videos do YT

---
+++ ytDownloader.py
@@ -0,0 +1,40 @@
+from pytube import YouTube
+from sys import argv
+
+#Seleciona se é audio se false é video
+baixa_audio_mp3 = True
+
+# PAssa URL AQUI EM ABIXO
+url = 'https://www.youtube.com/watch?v=BcuS8L_awqs&ab_channel=Techtrends'
+
+
+
+if (baixa_audio_mp3 == True) :
+   print("só audio")
+   my_video = YouTube(url)
+
+
+   yt = YouTube(url)
+
+   print ("Title: ", yt.title)
+
+   print ("View: ", yt.views)
+   my_video = my_video.streams.get_highest_resolution()
+   
+   # my_video = my_video.streams.filter(only_audio=True)
+   #  # yt.streams.filter(only_audio=True)
+   # [<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2" progressive="False" type="audio">,
+   # <Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus" progressive="False" type="audio">,
+   # <Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus" progressive="False" type="audio">,
+   # <Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus" progressive="False" type="audio">]
+
+   # stream = yt.streams.get_by_itag(251)
+   # stream.download("H:/videosss")
+   
+   my_video.download("H:/videosss")
+   print ("--------------------- ")
+   print ("--------------------- ")
+   print ("Download Finalizado: ")
+   
+   # stream = yt.streams.get_by_itag(22)
+   # >>> stream.download()
