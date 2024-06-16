639a3b499e4b3c2fdc26f3b483d78c3d0e2c3eba
everctba <everson777@gmail.com>
2024-06-16 19:32:03 -0300

    atualizado com registro de memoria endereço onteiro

--- sizeof.c
+++ sizeof.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#define TAM 3
 
+char vet[TAM];
+char a1;
+char a2;
+char a3;
+short  b;
+int c;
+long d;
+float e ;
+double f;
+long double  g;
 int main(void){
 printf("char ......: %d bytes\n", sizeof(char));
 printf("short .....: %d bytes\n", sizeof(short));
@@ -8,6 +19,22 @@ printf("long ......: %d bytes\n", sizeof(long));
 printf("float .....: %d bytes\n", sizeof(float));
 printf("double ....: %d bytes\n", sizeof(double));
 printf("long double: %d bytes\n", sizeof(long double));
+printf("char1 .....: %d bytes\n", &a1);
+printf("char2 .....: %d bytes\n", &a2);
+printf("char3 .....: %d bytes\n", &a3);
+printf("charvet0 ..: %d bytes\n", &vet[0]);
+printf("charvet1...: %d bytes\n", &vet[1]);
+printf("charvet2 ..: %d bytes\n", &vet[2]);
+printf("p_charvet0 : %p bytes\n", &vet[0]);
+printf("p_charvet1.: %p bytes\n", &vet[1]);
+printf("p_charvet2 : %p bytes\n", &vet[2]);
+
+printf("short .....: %p bytes\n", &b);
+printf("int .......: %p bytes\n", &c);
+printf("long ......: %p bytes\n", &d);
+printf("float .....: %p bytes\n", &e);
+printf("double ....: %p bytes\n", &f);
+printf("long double: %p bytes\n", &g);
 
 
 }
