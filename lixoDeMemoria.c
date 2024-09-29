8588f79b0110587af221fe71ee364f33ef10d6c3
everctba <everson777@gmail.com>
2024-09-29 02:08:36 -0300

    alterao para ver bit e hexa

--- lixoDeMemoria.c
+++ lixoDeMemoria.c
@@ -2,14 +2,19 @@
 #include<windows.h>
 int main() {
    while (1) {
-   unsigned char bytes [10];
-   Sleep(1000);
-   bytes[0] = 0b00000000;
+   unsigned char bytes [4] = {};
+   
+   bytes[0] = 0b00001110;
+   bytes[1] = 14;
+   bytes[2] = 0xE;
 
-   for (int i =0; i < 10; i ++) {
-      printf("[%2u] %3u", i, bytes[i]);
+   for (int i =0; i < 4; i ++) {
+      printf("[%2u] %2X", i, bytes[i]);
+      printf("  - [%2u] %3u", i, bytes[i]);
       printf("\n");
+     
 }
+ Sleep(5000);
 }
 return 0;
    }
